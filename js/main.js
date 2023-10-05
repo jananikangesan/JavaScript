a2869427733b24529996f74fe42551c94b66ec01
jananikangesan <janakanges1996@gmail.com>
2023-10-05 13:11:58 +0530

    Math code challenge

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Math method and properties
+
+//1 to 100
+console.log(Math.floor(Math.random()*100)+1);
+
+//displaying random letters in the given name
+const name="Computer Science";
+
+console.log(name.length)
+
+console.log(name.charAt(Math.floor(Math.random()*name.length)))
